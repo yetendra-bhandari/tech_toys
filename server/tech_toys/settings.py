5c77cdfc41d7c6f89837d7c770e24b97cc0eec40
Yetendra Bhandari <yetendrabhandari@gmail.com>
2024-06-18 02:04:48 +0530

    updated code

--- server/tech_toys/settings.py
+++ server/tech_toys/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'ariadne.contrib.django',
+    'ariadne_django',
     'api',
     'main'
 ]
--- server/tech_toys/urls.py
+++ server/tech_toys/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ariadne.contrib.django.views import GraphQLView
+from ariadne_django.views import GraphQLView
 from api.schema import schema
 
 urlpatterns = [
